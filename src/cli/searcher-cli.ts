17caff90368f48cf0ddf77a28cc5162f9aeea9b1
Vladlen Levchuk <l.v.sokrat@gmail.com>
2021-07-13 18:25:27 +0300

    searcher

--- src/cli/searcher-cli.ts
+++ src/cli/searcher-cli.ts
@@ -1,6 +1,16 @@
 import { cli } from '@debut/plugin-utils';
-import { DebutOptions, GenticWrapperOptions } from '@debut/types';
+import { DebutOptions, GenticWrapperOptions, DebutMeta, WorkingEnv } from '@debut/types';
 import { GeneticWrapper } from './tester/genetic';
+import { TesterTransport } from './tester/tester-transport';
+import { getHistory } from './tester/history';
+
+type Params = {
+    bot: string;
+    ticker: string;
+    days?: number;
+    ohlc?: boolean;
+    gap?: number;
+};
 
 type GeneticParams = {
     bot: string;
@@ -14,6 +24,8 @@ type GeneticParams = {
     ohlc?: boolean;
     gap?: number;
     best?: number;
+    daysForStudy?: number;
+    daysForTrade?: number;
 };
 
 const args = cli.getArgs() as GeneticParams;
@@ -29,6 +41,8 @@ const {
     ohlc,
     gap = 0,
     best = 5,
+    daysForStudy = 2,
+    daysForTrade = 1,
 } = args;
 
 const schema: cli.BotData | null = cli.getBotData(bot);
@@ -41,29 +55,66 @@ const schema: cli.BotData | null = cli.getBotData(bot);
 
     const { configs, meta } = schema;
     const config: DebutOptions = { ...configs[ticker], ticker, amount: Number(amount) };
-    const options: GenticWrapperOptions = {
-        days,
-        hours,
-        generations: gen,
-        populationSize: pop,
-        log,
-        ohlc,
-        gapDays: gap,
-        validateSchema: meta.validate,
-        score: meta.score,
-        stats: meta.stats,
-        create: meta.create,
-        ticksFilter: meta.ticksFilter,
-        best,
-    };
 
-    const genetic = new GeneticWrapper(options);
-    let stats = await genetic.start(meta.parameters, config);
+    for (let i = 0; i < days; i++) {
+        const options: GenticWrapperOptions = {
+            days: daysForStudy,
+            hours,
+            generations: gen,
+            populationSize: pop,
+            log,
+            ohlc,
+            gapDays: gap + i,
+            validateSchema: meta.validate,
+            score: meta.score,
+            stats: meta.stats,
+            create: meta.create,
+            ticksFilter: meta.ticksFilter,
+            best,
+        };
 
-    stats = stats.map((item, index) => {
-        item.config.id = index;
-        return item;
-    });
+        const genetic = new GeneticWrapper(options);
+        let stats = await genetic.start(meta.parameters, config);
 
-    console.log(stats[0].stats);
+        stats = stats.map((item, index) => {
+            item.config.id = index;
+            return item;
+        });
+
+        console.log(stats[0].config);
+
+        const cfg = stats[0].config;
+
+        test(cfg, meta);
+    }
 })();
+
+async function test(cfg: DebutOptions, meta: DebutMeta) {
+    try {
+        const transport = new TesterTransport({ ohlc, comission: cfg.fee, broker: cfg.broker, ticker: cfg.ticker });
+        const bot = await meta.create(transport, cfg, WorkingEnv.tester);
+        // const logger = new TesterLogger(transport);
+
+        if (!cfg) {
+            process.stdout.write('Genetic CLI error: Put config in bot cfgs.ts file');
+        }
+
+        const { broker = 'tinkoff', ticker, interval } = cfg;
+        let ticks = await getHistory({ broker, ticker, interval, days, gapDays: gap });
+
+        if (meta.ticksFilter) {
+            ticks = ticks.filter(meta.ticksFilter(cfg));
+        }
+
+        console.log('\n---- Tinkoff [' + cfg.ticker + '] ----\n');
+        console.log('Tested in ', ticks.length, ' candles...');
+        transport.setTicks(ticks);
+        await bot.start();
+        await transport.run();
+        await bot.closeAll();
+        await bot.dispose();
+        console.log(meta.stats(bot));
+    } catch (e) {
+        console.log(e);
+    }
+}
